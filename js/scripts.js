059f44b47183ad363ad32efa5e783e4d1c04c010
Jonathan Hernandez <jonhernandez91@gmail.com>
2020-08-13 12:39:21 -0700

    NPM Scripts Part 1

---
+++ js/scripts.js
@@ -0,0 +1,14 @@
+$(function() {
+    $(".carousel").carousel( { interval: 2000 } );
+    $("#carouselPause").click(function(){
+        if ($("#carouselPause").children("i").hasClass("fa-pause")) {
+            $(".carousel").carousel("pause");
+            $("#carouselPause").children("i").removeClass("fa-pause");
+            $("#carouselPause").children("i").addClass("fa-play");
+        } else {
+            $(".carousel").carousel("cycle");
+            $("#carouselPause").children("i").removeClass("fa-play");
+            $("#carouselPause").children("i").addClass("fa-pause");
+        }
+    });
+});
